49fd68edc5d4de27d24ecf734c9e990b37ffe761
SwarupKP127 <swarupkp12@gmail.com>
2022-02-20 19:23:39 +0530

    new project added

---
+++ demo-1/src/main/java/com/example/demo/utility/AppUtility.java
@@ -0,0 +1,10 @@
+package com.example.demo.utility;
+
+public class AppUtility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello india");
+	}
+
+}
